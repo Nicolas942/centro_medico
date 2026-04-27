c5c8d951bcfddaf4be999da06d183bd1ee41f854
Nicolas942 <nicolascabrera@colegioguanenta.edu.co>
2026-04-27 15:33:59 -0500

    act

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Centro médico CheloEPS</title>
+</head>
+<body>
+    <h1>Centro médico CheloEPS</h1>
+    <h2>Nicolás Alfonso Cabrera Suárez</h2>
+</body>
+</html>
