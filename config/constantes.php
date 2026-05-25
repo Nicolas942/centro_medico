789a37a65dd6d59999748305d97e458b2c7aebee
Nicolas942 <nicolascabrera@colegioguanenta.edu.co>
2026-05-25 14:45:16 -0500

    act

---
+++ config/constantes.php
@@ -0,0 +1,16 @@
+<?php
+    // Parametros requeridos para la conexion de la BD
+
+    // Parametros DB local - constantes
+    define('USER', 'acastillo'); // Crea la constante USER con valor 'root'
+    define('PW', '12345678');
+    define('HOST', 'localhost');
+    define('BD', 'centro_medico');
+
+    // Parametros DB remota - (infinityfree)
+    /*define('USER', 'if0_41712678'); 
+    define('PW', '1100959968');
+    define('HOST', 'andrescastillo.infinityfreeapp.com');
+    define('BD', 'centro_medico');*/
+
+?>
