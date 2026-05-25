d00a1b5d41c6e30a0f5c9d168d577407bc390b64
Nicolas942 <nicolascabrera@colegioguanenta.edu.co>
2026-05-25 14:50:21 -0500

    act

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Centro médico CheloEPS</title>
+    <title>Centro Medigo [G.D.A]</title>
 </head>
 <body>
-    <h1>Centro médico CheloEPS</h1>
-    <h2>Nicolás Alfonso Cabrera Suárez</h2>
+    <h1>Centro Medigo [G.D.A]</h1>
+    <h2>Andres Felipe Castilo Neira</h2>
 </body>
 </html>
